9333534b8110891cf75f6358222269fd02cc63a9
Guillaume Binet <gbin@google.com>
2015-08-10 11:50:28 -0700

    ported gen script to new naming

--- gen.sh
+++ gen.sh
@@ -18,29 +18,14 @@
 SRC=$GOPATH/src
 rm -Rf $SRC/google
 
-echo "gateway go"
+echo "GO: broker protos"
 protoc \
-  google/fakes/gateway.proto \
+  google/emulators/broker.proto \
   -I . --go_out=plugins=grpc:$SRC
 
-echo "protobuf go"
+echo "GO: protobuf"
 protoc \
   google/protobuf/*.proto \
   -I . --go_out=plugins=grpc:$GOPATH/src
 
-#echo "python"
-#rm -Rf python/google
-
-#protoc \
-#  google/fakes/gateway.proto \
-#  google/protobuf/*.proto \
-#-I . --python_out=python --grpc_out=python \
-#  --plugin=protoc-gen-grpc=`which grpc_python_plugin`
-
-#echo "additional python package fixups"
-#touch python/google/__init__.py
-#touch python/google/protobuf/__init__.py
-#touch python/google/fakes/__init__.py
-
-#mv python/google python/google_ext
 
