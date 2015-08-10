9779284f4cd649c788b96928db66b559855492e3
Guillaume Binet <gbin@google.com>
2015-08-10 06:57:44 -0700

    remove python for now.

--- gen.sh
+++ gen.sh
@@ -28,19 +28,19 @@ protoc \
   google/protobuf/*.proto \
   -I . --go_out=plugins=grpc:$GOPATH/src
 
-echo "python"
-rm -Rf python/google
+#echo "python"
+#rm -Rf python/google
 
-protoc \
-  google/fakes/gateway.proto \
-  google/protobuf/*.proto \
--I . --python_out=python --grpc_out=python \
-  --plugin=protoc-gen-grpc=`which grpc_python_plugin`
+#protoc \
+#  google/fakes/gateway.proto \
+#  google/protobuf/*.proto \
+#-I . --python_out=python --grpc_out=python \
+#  --plugin=protoc-gen-grpc=`which grpc_python_plugin`
 
-echo "additional python package fixups"
-touch python/google/__init__.py
-touch python/google/protobuf/__init__.py
-touch python/google/fakes/__init__.py
+#echo "additional python package fixups"
+#touch python/google/__init__.py
+#touch python/google/protobuf/__init__.py
+#touch python/google/fakes/__init__.py
 
-mv python/google python/google_ext
+#mv python/google python/google_ext
 
