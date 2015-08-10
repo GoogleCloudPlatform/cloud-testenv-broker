bdcc39f9e348f59c8c56cbd46100712b0ecfebe7
Guillaume Binet <gbin@google.com>
2015-08-10 14:24:14 -0700

    formatting

--- broker/server_test.go
+++ broker/server_test.go
@@ -23,14 +23,17 @@ func TestCreateSpec(t *testing.T) {
 		SpecId: "foo",
 		Spec:   want}
 	spec, err := s.CreateEmulatorSpec(nil, req)
+
 	if err != nil {
 		panic(err)
 	}
 
 	got, err := s.GetEmulatorSpec(nil, &emulators.SpecId{spec.Id})
+
 	if err != nil {
 		panic(err)
 	}
+
 	if got != want {
 		t.Errorf("Failed to find back the same spec want = %v, got %v", want, got)
 	}
@@ -52,16 +55,21 @@ func TestDoubleCreateSpec(t *testing.T) {
 		SpecId: "foo",
 		Spec:   want}
 	_, err := s.CreateEmulatorSpec(nil, req)
+
 	if err != nil {
 		panic(err)
 	}
+
 	spec, err := s.CreateEmulatorSpec(nil, req)
+
 	if err == nil {
 		t.Errorf("This creation should have failed.")
 	}
+
 	if grpc.Code(err) != codes.AlreadyExists {
 		t.Errorf("This creation should have failed with AlreadyExists.")
 	}
+
 	if spec != nil {
 		t.Errorf("It should not have returned a spec %q.", spec)
 	}
