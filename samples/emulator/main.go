7e4c9f1bbccdebb93dd43ff946e5fec9634bf653
Haw-Bin Chai <hbchai@google.com>
2015-08-11 15:08:05 -0400

    Emulator sample.

---
+++ samples/emulator/main.go
@@ -0,0 +1,130 @@
+/*
+Copyright 2015 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+  
+import (
+  "errors"
+  "flag"
+  "fmt"
+  "net/http"
+  "log"
+  "os"
+  "sync"
+  "time"
+
+  context "golang.org/x/net/context"
+  emulators "google/emulators"
+  grpc "google.golang.org/grpc"
+)
+
+var (
+  active  = flag.Bool("active", false, "Whether this emulator actively registers with the broker")
+  port       = flag.Int("port", 0, "The server port")
+  specId = flag.String("spec_id", "samples.emulator", "The id this emulator registers as")
+  wait = flag.Bool("wait", false, "Whether to wait for a request to '/setServing' before serving")
+)
+
+type statusServer struct {
+  serving bool
+  mux *http.ServeMux
+  mu sync.Mutex
+  servingCond *sync.Cond
+}
+
+func newStatusServer(serving bool) *statusServer {
+  s := statusServer{serving: serving, mux: http.NewServeMux()}
+  s.servingCond = sync.NewCond(&s.mu)
+  s.mux.HandleFunc("/status", func (w http.ResponseWriter, r *http.Request) {
+    s.mu.Lock()
+    if s.serving {
+      w.Write([]byte("ok\n"))
+    } else {
+      w.Write([]byte("bad\n"))
+    }
+    s.mu.Unlock()
+  })
+  s.mux.HandleFunc("/setServing", func(w http.ResponseWriter, r *http.Request) {
+    s.mu.Lock()
+    s.serving = true
+    s.servingCond.Broadcast()
+    s.mu.Unlock()
+  })
+  return &s
+}
+
+func (s* statusServer) waitUntilServing() {
+  s.servingCond.L.Lock()
+  for !s.serving {
+    s.servingCond.Wait()
+  }
+  s.servingCond.L.Unlock()
+}
+
+func registerWithBroker(brokerAddress string, myAddress string) error {
+  conn, err := grpc.Dial(brokerAddress, grpc.WithTimeout(1*time.Second))
+  if err != nil {
+    return errors.New(fmt.Sprintf("failed to dial broker: %v", err))
+  }
+  defer conn.Close()
+
+  ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+  spec := emulators.EmulatorSpec{Id: *specId, ResolvedTarget: myAddress}
+  broker := emulators.NewBrokerClient(conn)
+  _, err = broker.UpdateEmulatorSpec(ctx, &spec)
+  if err != nil {
+    return err
+  }
+  return nil
+}
+
+func main() {
+  flag.Parse()
+  if *port == 0 {
+    log.Fatalf("--port not specified")
+  }
+
+  // Start serving on port.
+  myAddress := fmt.Sprintf("localhost:%d", *port)
+  statusServer := newStatusServer(!*wait)
+  s := &http.Server{
+    Addr: myAddress,
+    Handler: statusServer.mux,
+  }
+  log.Printf("starting status server...")
+  done := make(chan bool, 1)
+  go func() {
+    s.ListenAndServe()
+    done <- true
+  }()
+  statusServer.waitUntilServing()
+  log.Printf("serving on port %d", *port)
+
+  if *active {
+    brokerAddress := os.Getenv("TESTENV_BROKER_ADDRESS")
+    if brokerAddress == "" {
+      log.Fatalf("TESTENV_BROKER_ADDRESS not specified")
+    }
+    err := registerWithBroker(brokerAddress, myAddress)
+    if err != nil {
+      log.Fatalf("failed to register with broker: %v", err)
+    }
+    log.Printf("registered with broker at %s", brokerAddress)
+  }
+
+  <- done
+}
+
